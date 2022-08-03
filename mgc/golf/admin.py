c742f4dae32aae80a83c40d6045f2787ec5dc6cd
jackfarrell2 <jackfarrell860@gmail.com>
2022-08-03 08:56:19 -0400

    initialized models.py

--- mgc/golf/admin.py
+++ mgc/golf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Round, Course, Hole
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Round)
+admin.site.register(Course)
+admin.site.register(Hole)
+
--- mgc/golf/models.py
+++ mgc/golf/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    tees = models.CharField(max_length=10, default="White")
+    par = models.IntegerField()
+    yardage = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.tees}"
+
+class Round(models.Model):
+    golfer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_rounds")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.golfer} - {self.course} - {self.date}"
+
+class Hole(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    tee = models.IntegerField()
+    par = models.IntegerField()
+    yardage = models.IntegerField()
+    handicap = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.tee} - {self.course}"
