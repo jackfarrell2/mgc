c2101c655ca184b3c24818f6f42f6bdedc2e56a9
jackfarrell2 <jackfarrell860@gmail.com>
2022-08-03 21:57:00 -0400

    Initialized layout.html, initialized login/out/register functionality

--- mgc/golf/models.py
+++ mgc/golf/models.py
@@ -2,10 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    name = models.CharField(max_length=50)
-
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.first_name}"
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
--- mgc/golf/urls.py
+++ mgc/golf/urls.py
@@ -4,5 +4,8 @@ from . import views
 # This is a test
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"), 
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
