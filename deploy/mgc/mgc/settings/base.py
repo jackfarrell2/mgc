880d3d6b47652dc19389376af6894a8ccc5887e4
Jack Farrell <jackfarrell860@gmail.com>
2023-12-18 20:37:44 -0500

    Adjusted API settings

--- deploy/mgc/mgc/settings/base.py
+++ deploy/mgc/mgc/settings/base.py
@@ -51,15 +51,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://137.184.117.146',
-    'http://localhost:3000',
-    'http://www.oldmanchestergolfclub.xyz',
-    'https://manchestergolfclub.com',
-    'https://www.manchestergolfclub.com',
-]
-
-
 ROOT_URLCONF = 'mgc.urls'
 
 TEMPLATES = [
