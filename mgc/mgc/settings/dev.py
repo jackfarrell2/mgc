383807fb6724be2fb5d235886071a1f9022d42ff
Jack Farrell <jackfarrell860@gmail.com>
2023-12-18 19:33:20 -0500

    Changed cors settings

--- mgc/mgc/settings/dev.py
+++ mgc/mgc/settings/dev.py
@@ -3,3 +3,10 @@ from .base import *
 SECRET_KEY = 'django-insecure-ezkvhf$85&5z^+qof@kq@8*5u!!bgb_qz)lma1#6luql)=vp+-'
 DEBUG = True
 ALLOWED_HOSTS = []
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://www.oldmanchestergolfclub.xyz',
+    'https://manchestergolfclub.com',
+    'https://www.manchestergolfclub.com',
+]
--- mgc/mgc/settings/prod.py
+++ mgc/mgc/settings/prod.py
@@ -6,3 +6,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost', '137.184.45.38',
                  'oldmanchestergolfclub.xyz', 'www.oldmanchestergolfclub.xyz']
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+CORS_ALLOWED_ORIGINS = [
+    'https://manchestergolfclub.com',
+    'https://www.manchestergolfclub.com',
+]
